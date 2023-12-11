bde9a2957d879aef43763b81c9099486eaa0fb95
D4Chub <akbievalim0578@gmail.com>
2023-12-12 01:17:04 +0300

    first

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://hd.10rdfilmhd.online'
+
+
+def get_soup():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    data = soup.find_all('div', class_='th-item')
+    for cards in data:
+        link = cards.find('a', class_='th-in with-mask').get('href')
+        image_link = url + cards.find('img').get('data-src')
+        name = cards.find('div', class_='th-title').text
+        rate = cards.find('div', class_='th-rate th-rate-kp').text
+        print('--------------------------------')
+        print(image_link, link, name, rate, sep='\n')
+
+
+get_soup()
+
+
+
